05a64ea70ac9dc63cd0f309c7f5f0afed6796efa
KevinLo <wind19891002@gmail.com>
2019-08-10 15:42:55 +0800

    Add compareListVC and compareListCell

---
+++ STYLiSH/Controller/CompareList/CompareListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CompareListViewController.swift
+//  STYLiSH
+//
+//  Created by 戴汝羽 on 2019/8/10.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class CompareListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ STYLiSH/View/CompareList/CompareListCell.swift
@@ -0,0 +1,18 @@
+//
+//  CompareListCell.swift
+//  STYLiSH
+//
+//  Created by 戴汝羽 on 2019/8/10.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class CompareListCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

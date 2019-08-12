e3ab9ed3df609f28ecdbff6e865fd4be1a0a8131
KevinLo <wind19891002@gmail.com>
2019-08-12 15:15:00 +0800

    Add NativeSignUpStoryboard, add Btn on Auth

---
+++ STYLiSH/Controller/Auth/NativeSignUpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NativeSignUpViewController.swift
+//  STYLiSH
+//
+//  Created by 戴汝羽 on 2019/8/12.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class NativeSignUpViewController: UIViewController {
+    
+    @IBAction func goBackPage(_ senber: UIBarButtonItem) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

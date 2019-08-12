141f8a291fcbe72e5fa0a18116d3e7e58b2342dd
KevinLo <wind19891002@gmail.com>
2019-08-12 16:29:10 +0800

    NativeSignUp Layout Done

--- STYLiSH/Controller/Auth/NativeSignUpViewController.swift
+++ STYLiSH/Controller/Auth/NativeSignUpViewController.swift
@@ -15,9 +15,64 @@ class NativeSignUpViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var passwordConfirmLabel: UILabel!
+    
+    @IBOutlet weak var userNameTextField: UITextField! {
+        didSet {
+            
+            userNameTextField.isEnabled = false
+        }
+    }
+    
+    @IBOutlet weak var userEmailTextField: UITextField!
+    
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    
+    @IBOutlet weak var userPasswordConfirmTextField: UITextField! {
+        didSet {
+            
+            userPasswordConfirmTextField.isEnabled = false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBOutlet weak var confirmBtn: UIButton! {
+        
+        didSet {
+            
+            confirmBtn.clipsToBounds = true
+            
+            confirmBtn.layer.cornerRadius = 5
+        }
+    }
+    
+    @IBAction func selectSignInOrSignUp(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            
+            userNameLabel.textColor = UIColor.B4
+            
+            passwordConfirmLabel.textColor = UIColor.B4
+            
+            userNameTextField.isEnabled = false
+            
+            userPasswordConfirmTextField.isEnabled = false
+            
+        } else {
+            
+            userNameLabel.textColor = UIColor.B1
+            
+            passwordConfirmLabel.textColor = UIColor.B1
+            
+            userNameTextField.isEnabled = true
+            
+            userPasswordConfirmTextField.isEnabled = true
+        }
+    }
 }
